83aa62d3e5405467a8c8cfa8cc63b7289f5be600
yahya18 <fuadyahya1812@gmail.com>
2021-06-13 20:34:23 +0700

    create repository

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Database\Seeds;
+
+class UserSeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        $data = [];
+        for($i=1; $i <= 10; $i++){
+            $data[] =[
+                'first_name'=> $faker->firstNameMale,
+                'last_name'=> $faker->lastName,
+                'username'=> $faker->username,
+                'email'=> $faker-> email,
+                'password'=> password_hash('12345', PASSWORD_DEFAULT)
+            ];
+        }
+        $this->db->table('users')->insertBatch($data);
+    }
+}
+
+?>
